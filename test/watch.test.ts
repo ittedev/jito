e5ee470a70349ea668162fbb25e249ebcf60fa10
itte1 <57395168+itte1@users.noreply.github.com>
2021-11-25 21:04:52 +0000

    create watch object

---
+++ test/watch.test.ts
@@ -0,0 +1,35 @@
+import { assertEquals } from 'https://deno.land/std/testing/asserts.ts'
+import { watch } from '../watch.ts'
+
+Deno.test('watch object', () => {
+  const data = {
+    x: 1
+  }
+  watch(data)
+  assertEquals(data.x, 1)
+  data.x = 2
+  assertEquals(data.x, 2)
+  assertEquals(JSON.stringify(data), '{"x":2}')
+})
+
+Deno.test('watch object exec function', () => {
+  const data = {
+    x: 1
+  }
+  let y = 0
+  watch(data, 'x', (v: number) => { y = v })
+  data.x = 2
+  assertEquals(y, 2)
+})
+
+Deno.test('watch nested object', () => {
+  const data = {
+    x: {
+      x: 1
+    }
+  }
+  let y = 0
+  watch(data, 'x.x', (v: number) => { y = v })
+  data.x.x = 2
+  assertEquals(y, 2)
+})
--- watch.ts
+++ watch.ts
@@ -1,2 +1,60 @@
-export const watch = (target:object, path?: string, fund?: Function): void => {
+interface DictionaryPage {
+  v: unknown
+  f: Set<Callback>
+}
+
+interface Dictionary {
+  [key: string]: DictionaryPage
+}
+
+const dictionary = Symbol()
+
+interface BearkoObject {
+  [key: string]: unknown
+  [dictionary]: Dictionary
+}
+
+// deno-lint-ignore no-explicit-any
+type Callback = (newValue: any, oldValue: any) => void
+
+export const watch = (data: unknown, path?: string | string[], callback?: Callback): unknown => {
+  if (typeof data === 'object' && data !== null) {
+    if (Array.isArray(data)) {
+      // TODO: watch Array
+      return data
+    } else {
+      const pathes = callback && path ? [path].flat() : []
+      const obj = data as BearkoObject
+      if (!(dictionary in obj)) {
+        obj[dictionary] = {} as Dictionary
+      }
+      for (const key in obj) {
+        if (!(key in obj[dictionary])) {
+          obj[dictionary][key] = {
+            v: watch(
+              obj[key],
+              pathes.flatMap(path => path.startsWith(key + '.') ? path.slice(key.length + 1) : []),
+              callback
+            ),
+            f: new Set<Callback>()
+          }
+          Object.defineProperty(obj, key, {
+            get: function() { return this[dictionary][key].v },
+            set: function(val) {
+              const page = this[dictionary][key] as DictionaryPage
+              const old = page.v
+              page.v = watch(val)
+              if (old !== val) {
+                page.f.forEach(callback => callback(val, old))
+              }
+            }
+          })
+        }
+        if (pathes.some(path => path === key)) {
+          obj[dictionary][key].f.add(callback as Callback)
+        }
+      }
+    }
+  }
+  return data
 }
