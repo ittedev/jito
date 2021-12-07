5248b151ccbbb5dfa3d71fd145d05c03d826989c
itte1 <57395168+itte1@users.noreply.github.com>
2021-12-07 13:41:20 +0000

    update document console

--- _helper/document_console.ts
+++ _helper/document_console.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore no-explicit-any
 export function log(id: string, ...args: any[]) {
-  console.log(...args)
+  console.log(id, ...args)
   const text = args.map(arg => typeof arg === 'object' ? JSON.stringify(arg) : arg).join(' ')
   const p = document.createElement('p')
   p.id = id
