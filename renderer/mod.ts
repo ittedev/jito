1186d5dac1baabc9d21262bb0cd6178ad0209656
itte1 <57395168+itte1@users.noreply.github.com>
2021-12-11 18:03:24 +0000

    remove renderer files

--- renderer/mod.ts
+++
@@ -1,3 +0,0 @@
-// Copyright 2021 itte.dev. All rights reserved. MIT license.
-
-export { render } from './render.ts'
--- renderer/render.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2021 itte.dev. All rights reserved. MIT license.
-
-import { Template } from '../generator/types.ts'
-
-export function render(template: string | Template, data?: Object): string {
-  return ''
-}
