332c7c9e6cb0c10f6bc902267b483280cafd31f9
itte1 <57395168+itte1@users.noreply.github.com>
2022-01-09 03:12:32 +0000

    remove target document

--- web_components/hack.ts
+++ web_components/hack.ts
@@ -24,23 +24,12 @@ export function hack(element: Element, template: TreeTemplate): void
 export function hack(element: Element, template: TreeTemplate, stack: Variables): void
 export function hack(element: Element, template: TreeTemplate, stack: Record<string, unknown>): void
 export function hack(element: Element, template: TreeTemplate, construct: ComponentConstructor): void
-export function hack(doc: Document, component: Component): void
-export function hack(doc: Document, html: string): void
-export function hack(doc: Document, html: string, stack: Variables): void
-export function hack(doc: Document, html: string, stack: Record<string, unknown>): void
-export function hack(doc: Document, html: string, construct: ComponentConstructor): void
-export function hack(doc: Document, template: TreeTemplate): void
-export function hack(doc: Document, template: TreeTemplate, stack: Variables): void
-export function hack(doc: Document, template: TreeTemplate, stack: Record<string, unknown>): void
-export function hack(doc: Document, template: TreeTemplate, construct: ComponentConstructor): void
-export function hack(element: string | Element | Document, template: string | TreeTemplate | Component, stack: Variables | Record<string, unknown> | ComponentConstructor): void
-export function hack(element: string | Element | Document, template: string | TreeTemplate | Component, stack: Variables | Record<string, unknown> | ComponentConstructor = []): void {
+export function hack(target: string | Element, template: string | TreeTemplate | Component, stack: Variables | Record<string, unknown> | ComponentConstructor): void
+export function hack(target: string | Element, template: string | TreeTemplate | Component, stack: Variables | Record<string, unknown> | ComponentConstructor = []): void {
   const root: Element =
-    typeof element === 'string' ?
-      document.querySelector(element) as Element :
-    element.nodeType === 9 ? // DOCUMENT_NODE
-      (element as Document).body :
-      element as Element
+    typeof target === 'string' ?
+      document.querySelector(target) as Element :
+      target as Element
   const tree = load(root.attachShadow({mode: 'open'}))
   const component = instanceOfComponent(template) ? template : compact(template, stack)
   new Entity(component, root, tree)
