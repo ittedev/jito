31b0108e362b5948e10b8e4fffcd2bd2827d2c21
Nakamura <57395168+itte1@users.noreply.github.com>
2022-01-28 09:58:17 +0000

    Remove html func

--- template_engine/html.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2022 itte.dev. All rights reserved. MIT license.
-// This module is browser compatible.
-export function html(templates: TemplateStringsArray): string {
-  return templates.raw[0]
-}
