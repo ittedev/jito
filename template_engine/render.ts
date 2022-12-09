f5ac7ee51f898ab952a8b0c5043846ff3fcb45e5
itte1 <nakamura@itte.dev>
2022-12-09 09:43:48 +0900

    Add render function

---
+++ template_engine/render.ts
@@ -0,0 +1,30 @@
+import {
+  VirtualTree,
+  VirtualNode,
+  VirtualElement
+} from '../virtual_dom/types.ts'
+
+export function render(tree: VirtualTree): string
+{
+  return tree.children ? renderChildren(tree.children) : ''
+}
+
+// Not support RealTarget
+export function renderChildren(children: VirtualNode[]): string
+{
+  return children
+    .filter(child => typeof child !== 'number')
+    .map(child => typeof child === 'string' ? child : renderElement(child as VirtualElement))
+    .join('')
+}
+
+export function renderElement(ve: VirtualElement): string
+{
+  let idSection = ve.is ? ` is="${ve.is}"` : ''
+  let classSection = ve.class ? ` class="${ve.class.join(' ')}"` : ''
+  let partSection = ve.part ? ` part="${ve.part.join(' ')}"` : ''
+  let styleSection = ve.style ? ` style="${ve.style}"` : ''
+  let attrsSection = ve.attrs ? Object.entries(ve.attrs).map(attr => ` ${attr[0]}="${attr[1]}"`).join('') : ''
+  let childrenSection = ve.children ? renderChildren(ve.children) : ''
+  return '<' + ve.tag + idSection + classSection + partSection + styleSection + attrsSection + `>${childrenSection}</${ve.tag}>`
+}
