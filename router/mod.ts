377e9f04ca0c6ffe46bc09e03bd13a653b8af454
Nakamura <57395168+itte1@users.noreply.github.com>
2023-05-22 14:15:16 +0000

    Sort

--- router/mod.ts
+++ router/mod.ts
@@ -1,3 +1,3 @@
-export { Panel } from './panel.ts'
-export { Router } from './router.ts'
 export { MemoryHistory } from './memory_history.ts'
+export { Router } from './router.ts'
+export { Panel } from './panel.ts'
